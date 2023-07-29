f6b73e77fd8b1ca90e4a95bd26913011a9e36058
Ashakuur1 <engshakrayare114@gmail.com>
2023-07-29 23:08:37 +0300

    hackathon project

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -3,6 +3,7 @@ import { information } from "../Data";
 const Donate = () => {
   return (
     <div className="container">
+        <h1>Hospital's Need donate</h1>
       {information.map((info) => (
         <Section infor={info} key={info.id} />
       ))}
